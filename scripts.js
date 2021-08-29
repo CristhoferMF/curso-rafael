74ad46126eb806b7d38796cf06c98d684a884f49
Cristhofer Montalvo <cristhofermontalvo@gmail.com>
2021-08-29 18:00:20 -0500

    Styles v1

--- scripts.js
+++ scripts.js
@@ -3,15 +3,20 @@
 const form = document.getElementById("pokemon-form");
 const API_URL = "https://pokeapi.co/api/v2/pokemon/"
 
+const elementName = document.getElementById("pokemon-name");
+const elementImage = document.getElementById("pokemon-image")
+
 form.addEventListener("submit", function (element) {
     element.preventDefault();
     
     const inputId = form[0];
     const id = inputId.value;
+    fetchPokemon(id);
+})
+
+function fetchPokemon(id){
     let name = "";
-    const elementName = document.getElementById("pokemon-name");
-    const elementImage = document.getElementById("pokemon-image")
-    
+
     fetch(API_URL+id).then(function (response){
         return response
     }).then( function (response) {
@@ -23,5 +28,8 @@ form.addEventListener("submit", function (element) {
     }).catch(function () {
         alert("No se encontró el pokemon")
     })
+}
 
+document.addEventListener("DOMContentLoaded", function() {
+    //fetchPokemon(150)
 })
